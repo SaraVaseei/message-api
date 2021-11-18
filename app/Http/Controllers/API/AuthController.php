0db4e9fe0cd22988a8ca939c5fd7a4c3a7ea1620
sara.vaseei <sara.vaseei@peykasa.ir>
2021-11-18 12:38:26 +0330

    Add swagger to auth endpoints

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\RegisterUserRequest;
@@ -14,6 +14,56 @@ class AuthController extends Controller
     /**
      * @param RegisterUserRequest $request
      * @return mixed
+     */
+    /**
+     * @OA\Post(
+     *      path="/api/register",
+     *      operationId="registerUser",
+     *      tags={"user"},
+     *      summary="Register a new user",
+     *      description="Returns new user data",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  required={"name", "email", "password", "password_confirmation"},
+     *                  @OA\Property(
+     *                      property="name",
+     *                      type="string",
+     *                      description="Name"
+     *                  ),@OA\Property(
+     *                      property="email",
+     *                      type="string",
+     *                      description="Email"
+     *                  ),@OA\Property(
+     *                      property="password",
+     *                      type="string",
+     *                      description="Password"
+     *                  ),@OA\Property(
+     *                      property="password_confirmation",
+     *                      type="string",
+     *                      description="Password confirmation"
+     *                  ),
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=405,
+     *          description="Method Not Allowed"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Server error"
+     *      )
+     * )
      */
     public function register(RegisterUserRequest $request)
     {
@@ -40,6 +90,48 @@ class AuthController extends Controller
     /**
      * @param LoginUserRequest $request
      * @return mixed
+     */
+    /**
+     * @OA\Post(
+     *      path="/api/login",
+     *      operationId="loginUser",
+     *      tags={"user"},
+     *      summary="User login",
+     *      description="Returns user data",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  required={"email", "password"},
+     *                  @OA\Property(
+     *                      property="email",
+     *                      type="string",
+     *                      description="Email"
+     *                  ),@OA\Property(
+     *                      property="password",
+     *                      type="string",
+     *                      description="Password"
+     *                  ),
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=405,
+     *          description="Method Not Allowed"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Server error"
+     *      )
+     * )
      */
     public function login(LoginUserRequest $request)
     {
@@ -71,6 +163,44 @@ class AuthController extends Controller
     /**
      * @param Request $request
      * @return mixed
+     */
+    /**
+     * @OA\Post(
+     *      path="/api/logout",
+     *      operationId="logoutUser",
+     *      tags={"user"},
+     *      security={{"bearerAuth":{}}},
+     *      summary="User logout",
+     *      description="Returns new user data",
+     *      @OA\RequestBody(
+     *          required=false,
+     *          @OA\MediaType(mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  required={},
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *          response=405,
+     *          description="Method Not Allowed"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Server error"
+     *      )
+     * )
      */
     public function logout(Request $request)
     {
